361096e8152eaf2f9774f4f5650ddf89102dbc8b
Ivan Sanchez <ivan.sanchez3@gmail.com>
2022-02-16 00:23:35 +0100

    Feature/logout idp filter (#1)
    * [ADD] liferay-logout-filter-openid: init workspace Liferay DXP 7.1 with gradle
    
    * [IMP] logout-idp-filter: complete functionality to sample logout filter for openid connect to anyone IDP

---
+++ modules/logout-idp-filter/src/main/java/com/sample/logout/idp/filter/config/LogoutIdpFilterConfiguration.java
@@ -0,0 +1,28 @@
+package com.sample.logout.idp.filter.config;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.sample.logout.idp.filter.constants.LogoutIdpFilterKeys;
+
+/**
+ * @author Ivan Sánchez
+ */
+@ProviderType
+@ExtendedObjectClassDefinition(
+        category = LogoutIdpFilterKeys.CONFIGURATION_CATEGORY,
+        scope = ExtendedObjectClassDefinition.Scope.SYSTEM
+)
+@Meta.OCD(
+        id = LogoutIdpFilterKeys.WIDGET_CONFIGURATION_NAME,
+        localization = LogoutIdpFilterKeys.CONFIGURATION_LOCALIZATION
+)
+public interface LogoutIdpFilterConfiguration {
+
+    @Meta.AD(
+            deflt = "Keycloak",
+            required = false
+    )
+    String idpName();
+
+}
---
+++ modules/logout-idp-filter/src/main/java/com/sample/logout/idp/filter/constants/LogoutIdpFilterKeys.java
@@ -0,0 +1,16 @@
+package com.sample.logout.idp.filter.constants;
+
+/**
+ * @author Ivan Sánchez
+ */
+public class LogoutIdpFilterKeys {
+
+	public static final String WIDGET_CONFIGURATION_NAME = "com.sample.logout.idp.filter.config.LogoutIdpFilterConfiguration";
+	public static final String CONFIGURATION_CATEGORY = "LogoutUrl IDP Filter configuration";
+	public static final String CONFIGURATION_LOCALIZATION = "content/language";
+
+    public static final String PARAM_AUTH_ENDPOINT = "authorization_endpoint";
+	public static final String PARAM_AUTH = "/auth";
+	public static final String PARAM_LOGOUT = "/logout";
+	public static final String REDIRECT_URI = "?redirect_uri=";
+}
